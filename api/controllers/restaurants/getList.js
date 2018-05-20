a144b33ffd9644dc3f34e94d4ce05898b9c83787
Natalia Voronina <natalibriks87@gmail.com>
2018-05-20 12:33:12 +0300

    Auth fix

--- api/controllers/restaurants/getList.js
+++ api/controllers/restaurants/getList.js
@@ -3,7 +3,7 @@ const restoranModel = require('./../../models/restoranModel');
 
 module.exports.getList = async (req, res)=>{
 	try {
-		const info = await restoranModel.getAll(25);
+		const info = await restoranModel.getAll(10000);
 		console.log(info);
 		if (info._status) {
 			res.status(info._status).send(info);
